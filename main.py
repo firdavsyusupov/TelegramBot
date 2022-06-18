add7b6b0cb9d4e83fbb5255c574b0efdfc555dfa
Firdavs Yusupov <84022174+firdavsyusupov@users.noreply.github.com>
2022-06-18 10:22:58 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,36 +3,71 @@ import config
 import au
 
 bot = telebot.TeleBot(config.TOKEN)
+#ID users
 joinedFile = open("joined.txt", "r")
 joinedUsers = set()
 for line in joinedFile:
     joinedUsers.add(line.strip())
 joinedFile.close()
+#Log users(ID,FIRST_NAME,LAST_NAME,USERNAME)
+logFile = open("log.txt", "r")
+logUsers = set()
+for data in logFile:
+    logUsers.add(data.strip())
+logFile.close()
+
 
 
 # Greeting
 @bot.message_handler(commands=['start'])
 def welcome(message):
     au = open('info/start.ogg', 'rb')
-    bot.send_audio(message.chat.id, au, "{0.first_name}, если ты услышал(а) это приветствие, значит начиная со следующего, ты будешь ежедневно получать обещанные послания в формате голосовых сообщений 🎧\n\n<b>Как только я запишу что-то новое, я обязательно тебе отправлю</b> 😊".format(message.from_user, bot.get_me()), parse_mode='html')
-    print("Пользователь запустил бота:\n" + "Name: " + "\nSurname: " + "\nUsername: ")
+    bot.send_audio(message.chat.id, au,"{0.first_name}, если ты услышал(а) это приветствие, значит начиная со следующего, ты будешь ежедневно получать обещанные послания в формате голосовых сообщений 🎧\n\n<b>Как только я запишу что-то новое, я обязательно тебе отправлю</b> 😊".format(message.from_user, bot.get_me()), parse_mode='html')
+    print("Пользователь запустил бота:\n" + str(message.from_user.id), str(message.from_user.first_name), str(message.from_user.username))
+
     if not str(message.chat.id) in joinedUsers:
         joinedFile = open("joined.txt", "a")
         joinedFile.write(str(message.chat.id) + '\n')
         joinedUsers.add(message.chat.id)
+
+    if not str(message.from_user.id) and str(message.from_user.first_name) in logUsers:
+        logFile = open("log.txt", "a")
+        logFile.write(str(message.from_user.id, message.from_user.first_name) + '\n')
+        logUsers.add(message.form_user.id, message.from_user.first_name)
 
 # New post for users
 @bot.message_handler(commands=['sendnewpost'])
 def newpost(message):
     for user in joinedUsers:
         au = open('info/1.ogg', 'rb')
-        #bot.send_message(user, message).text[message.text.find(' '):]
-        bot.send_audio(user, au, "Это сегодня ☝️😍\n\n<a href='http://t.me/yupgrade_bot'>© Твой Апгрейд ♥</a> \n\nПоделись с друзьями 🥺".format(message.from_user, bot.get_me()), parse_mode='html')
-        print("Сообщение успешно отправлено!")
+        # bot.send_message(user, message).text[message.text.find(' '):]
+        bot.send_audio(user, au,
+                       "Это сегодня ☝️😍\n\n<a href='http://t.me/yupgrade_bot'>© Твой Апгрейд ♥</a> \n\nПоделись с друзьями 🥺".format(
+                           message.from_user, bot.get_me()), parse_mode='html')
+        print("Сообщение успешно отправлено")
 
-# Info
-'''
-log = ["{first_name}", "{last_name}"]
+
+@bot.message_handler(commands=['sendnewpost2'])
+def newpost2(message):
+    for i in joinedUsers:
+        au = open('info/2.ogg', 'rb')
+        bot.send_audio(i, au, "Удачи :)")
+        print("Сообщение успешно отправлено_2")
+
+
+@bot.message_handler(commands=['testbot'])
+def testbot(message):
+    bot.send_message(message.chat.id, "Бот работает :)\n")
+    print("Бот работает :D")
+
+
+# Log
+@bot.message_handler(commands=['info'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Бот, который ежедневно отправляет подбадривающие и мотивирующие голосовые сообщения от реального человека :)\n\n🎙| Бота озвучивает – Ronny*****\n👤| Сотрудничество – <a href='http://t.me/site_com_uz_officeal'>@site_com_uz_official</a>\nДата создания бота: 16.06.2022".format(message.from_user, bot.get_me()), parse_mode='html')
+
+    """
+x = ["{first_name}", "{last_name}"]
     file = open("log.txt", 'w+')
     for i in log:
         file.write(i + '\n')
@@ -45,8 +80,10 @@ def info(message):
 @bot.message_handler(content_types=['text'])
 def lalala(message):
     bot.send_message(message.chat.it, message.text)
-'''
+"""
 
 
 # Run bot
-bot.polling(none_stop=True)
+# bot.polling(none_stop=True)
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
--- test.py
+++ test.py
@@ -1,12 +1,76 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+import sqlite3
+from sqlite3 import Error
+from time import sleep, ctime
+import telebot
+from telebot import types
+import pandas as pd
+
+bot = telebot.TeleBot('5483326295:AAGA6BUhZPFIVgA93Ql_Kql3i7Y2RTQNXUo')
+id = str(id)
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+    item1 = types.KeyboardButton("кнопка")
 
 
-Base = declarative_base()
+    markup.add(item1)
+
+    bot.send_message(message.chat.id, "Привет, {0.first_name} !".format(message.from_user), 
+    reply_markup = markup)
+
+@bot.message_handler (content_types = ["text"])
+def bot_message(message):
+    if message.chat.type == "private":
+        if message.text == "Бонус💣":
+            bot.send_message(message.chat.id, "сообщение")
 
 
-class MediaIds(Base):
-    __tablename__ = 'Media ids'
-    id = Column(Integer, primary_key=True)
-    file_id = Column(String(255))
-    filename = Column(String(255))
+def post_sql_query(sql_query):
+    with sqlite3.connect('data.db') as connection:
+        cursor = connection.cursor()
+    try:
+        cursor.execute(sql_query)
+    except Error:
+        pass
+    result = cursor.fetchall()
+    return result
+
+def create_tables():
+    users_query = '''CREATE TABLE IF NOT EXISTS USERS 
+                    (user_id INTEGER PRIMARY KEY NOT NULL,
+                    username TEXT,
+                    first_name TEXT,
+                    last_name TEXT,
+                    reg_date TEXT);'''
+    post_sql_query(users_query)
+
+def register_user(user, username, first_name, last_name):
+    user_check_query = f'SELECT * FROM USERS WHERE user_id = {user};'
+    user_check_data = post_sql_query(user_check_query)
+    if not user_check_data:
+     insert_to_db_query = f'INSERT INTO USERS (user_id, username, first_name,  last_name, reg_date) VALUES ({user}, "{username}", "{first_name}", "{last_name}", "{ctime()}");'
+     post_sql_query(insert_to_db_query )
+
+    create_tables()
+
+f = open('output.txt', 'w')
+connection = sqlite3.connect('data.db')
+cursor = connection.cursor()
+cursor.execute('select * from USERS')
+while True:
+    df = pd.DataFrame(cursor.fetchmany(1000))
+    if len(df) == 0:
+        break
+
+else:
+    df.to_txt(f, header=False)
+
+
+f.close()
+cursor.close()
+connection.close()
+
+
+
+bot.polling(none_stop = True)   
